f35c51cf98da032b605bc825d10e8d55f9e02d5b
John Boutros <john.g.boutros@gmail.com>
2018-01-03 01:43:28 +0200

    JB: MediaRecorder demo + Manifest permissions

--- app/src/main/java/com/gypsian/android/samples/voicemessages/MessengerActivity.java
+++ app/src/main/java/com/gypsian/android/samples/voicemessages/MessengerActivity.java
@@ -1,13 +1,72 @@
 package com.gypsian.android.samples.voicemessages;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
 
 public class MessengerActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "MessengerActivity";
+
+    private Button mButton;
+    private MediaRecorder mRecorder;
+
+    private String mFileName = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Record to the external cache directory for visibility
+        mFileName = getExternalCacheDir().getAbsolutePath();
+        mFileName += "/audiorecordtest.3gp";
+
         setContentView(R.layout.activity_messenger);
+
+        mButton = (Button) findViewById(R.id.btn_record);
+
+        mButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        startRecording();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        stopRecording();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
     }
 }
